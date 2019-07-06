77d3cafe545249acca9a544605c56027bedaf282
tete <tiagomdepaula@gmail.com>
2019-07-06 14:29:51 -0300

    Remove comment.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,15 +10,6 @@ use App\Models\Role;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
 
     public function login(Request $request)
     {
